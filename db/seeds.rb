62fdc304052f8e178e8750f08a4c868cdbb4c5b7
Joseph VICENT <vicent.jsp@gmail.com>
2020-06-23 16:39:30 +0200

    Branch not usefull

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,97 +1,12 @@
 require 'open-uri'
 require 'nokogiri'
 
-article_fill = true
-video_fill = true
-coach_fill = true
-
-# articles
-if article_fill
-  rank = 1
-  puts "please wait, we fill the article db"
-  Article.destroy_all
-  puts "all articles are destroy, but don't panic"
-  urls = ["https://www.pole-emploi.fr/candidat/votre-projet-professionnel/evaluer-vos-competences/levaluation-des-competences-et-d.html",
-          "https://www.pole-emploi.fr/candidat/votre-projet-professionnel/definir-votre-projet-professionn/le-conseil-en-evolution-professi.html",
-          "https://www.pole-emploi.fr/candidat/en-formation/definir-vos-besoins/choisir-votre-formation.html",
-          "https://www.pole-emploi.fr/region/occitanie/candidat/formation/une-formation-pour-quoi-faire.html",
-          "https://www.pole-emploi.fr/candidat/vos-recherches/bien-vous-organiser/3-idees-pour-enrichir-votre-parc.html",
-          "https://www.pole-emploi.fr/region/auvergne-rhone-alpes/evenements/1-semaine-pour-1-emploi/employeurs-pourquoi-et-comment-p.html",
-          "https://www.pole-emploi.fr/employeur/vos-recrutements/integrer-un-nouveau-salarie/entretien-tutorat-parrainage-pri.html"
-        ]
-  base = "https://www.pole-emploi.fr"
-  urls.each do |url|
-    article = Article.new
-    html_file = open(url).read
-    html_doc = Nokogiri::HTML(html_file)
-    paragraphes = []
-    element = html_doc.search('.container')[2]
-    titre = element.search('h1').text.strip
-    puts titre
-    article.title = titre
-    element.search('p').each do |paragraphe|
-      paragraphes << paragraphe.text.strip if paragraphe.text.strip.size > 100
-    end
-    puts paragraphes
-    article.content = paragraphes.join(" ")
-    if element.search('img').attribute('src')
-      image = element.search('img').attribute('src') 
-      url_image = base + image
-      puts url_image
-      article.image = url_image
-    end
-    puts rank
-    article.step_id = rank
-    article.save!
-    rank += 1
-  end
-  puts "Now the article db is fill"
-end
-
-# video youtube:
-if video_fill
-  rank =1
-  puts "please wait, we fill the video db"
-  Video.destroy_all
-  puts "all videos are destroy, but don't panic"
-  youtube_url = ["https://www.youtube.com/embed/jpPL_5dHGSA",
-                "https://www.youtube.com/embed/PMnTR-8XVHg",
-                "https://www.youtube.com/embed/v5um5glCEzg",
-                "https://www.youtube.com/embed/hkVFPpElRjk",
-                "https://www.youtube.com/embed/WUKzm250bU8",
-                "https://www.youtube.com/embed/91EZeR5tbos?list=PLi6L3Jci2WszzB8Nmr_rKoT7UDyQAt4VX",
-                "https://www.youtube.com/embed/fzqwEcIaauk"]
-  youtube_url.each do |url|
-    video = Video.new
-    video.url = url
-    video.step_id = rank
-    puts video.url
-    video.save!
-    rank += 1
-  end
-  puts "Now the video db is fill"
-end
-
-# coaching:
-if coach_fill
-  puts "please wait, we fill the coach db"
-  Coach.destroy_all
-  puts "all coachs are destroy, but don't panic"
-  7.times do
-    coach = Coach.new
-    coach.email = Faker::Name.first_name + "@upya.fr"
-    puts coach.email
-    coach.save!
-  end
-  puts ">The coach db is fill"
-end
-
 puts "> Destroy is comming !"
 
 User.destroy_all
 Choice.destroy_all
 Question.destroy_all
-Step.destroy_all
+# Step.destroy_all
 
 puts "> Destroy is finish !"
 
@@ -187,3 +102,90 @@ user9 = User.create!(email: "pablo@user.com", password: "azerty", name: "Pablo",
 user10 = User.create!(email: "gustavo@user.com", password: "azerty", name: "Gustavo", current_question_id: question1.id, step_id: step1.id)
 
 puts "> All the users are created !"
+
+########################################### CONTENT ############################################
+
+article_fill = true
+video_fill = true
+coach_fill = true
+
+### ARTICLES ###
+if article_fill
+  rank = Step.first.id
+  puts "please wait, we fill the article db"
+  Article.destroy_all
+  puts "all articles are destroy, but don't panic"
+  urls = ["https://www.pole-emploi.fr/candidat/votre-projet-professionnel/evaluer-vos-competences/levaluation-des-competences-et-d.html",
+          "https://www.pole-emploi.fr/candidat/votre-projet-professionnel/definir-votre-projet-professionn/le-conseil-en-evolution-professi.html",
+          "https://www.pole-emploi.fr/candidat/en-formation/definir-vos-besoins/choisir-votre-formation.html",
+          "https://www.pole-emploi.fr/region/occitanie/candidat/formation/une-formation-pour-quoi-faire.html",
+          "https://www.pole-emploi.fr/candidat/vos-recherches/bien-vous-organiser/3-idees-pour-enrichir-votre-parc.html",
+          "https://www.pole-emploi.fr/region/auvergne-rhone-alpes/evenements/1-semaine-pour-1-emploi/employeurs-pourquoi-et-comment-p.html",
+          "https://www.pole-emploi.fr/employeur/vos-recrutements/integrer-un-nouveau-salarie/entretien-tutorat-parrainage-pri.html"
+        ]
+  base = "https://www.pole-emploi.fr"
+  urls.each do |url|
+    article = Article.new
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    paragraphes = []
+    element = html_doc.search('.container')[2]
+    titre = element.search('h1').text.strip
+    puts titre
+    article.title = titre
+    element.search('p').each do |paragraphe|
+      paragraphes << paragraphe.text.strip if paragraphe.text.strip.size > 100
+    end
+    puts paragraphes
+    article.content = paragraphes.join(" ")
+    if element.search('img').attribute('src')
+      image = element.search('img').attribute('src')
+      url_image = base + image
+      puts url_image
+      article.image = url_image
+    end
+    puts rank
+    article.step_id = rank
+    article.save!
+    rank += 1
+  end
+  puts "Now the article db is fill"
+end
+
+### VIDEO YOUTUBE ###
+if video_fill
+  rank = Step.first.id
+  puts "please wait, we fill the video db"
+  Video.destroy_all
+  puts "all videos are destroy, but don't panic"
+  youtube_url = ["https://www.youtube.com/embed/jpPL_5dHGSA",
+                "https://www.youtube.com/embed/PMnTR-8XVHg",
+                "https://www.youtube.com/embed/v5um5glCEzg",
+                "https://www.youtube.com/embed/hkVFPpElRjk",
+                "https://www.youtube.com/embed/WUKzm250bU8",
+                "https://www.youtube.com/embed/91EZeR5tbos?list=PLi6L3Jci2WszzB8Nmr_rKoT7UDyQAt4VX",
+                "https://www.youtube.com/embed/fzqwEcIaauk"]
+  youtube_url.each do |url|
+    video = Video.new
+    video.url = url
+    video.step_id = rank
+    puts video.url
+    video.save!
+    rank += 1
+  end
+  puts "Now the video db is fill"
+end
+
+### COACHING ###
+if coach_fill
+  puts "please wait, we fill the coach db"
+  Coach.destroy_all
+  puts "all coachs are destroy, but don't panic"
+  7.times do
+    coach = Coach.new
+    coach.email = Faker::Name.first_name.downcase + "@upya.fr"
+    puts coach.email
+    coach.save!
+  end
+  puts ">The coach db is fill"
+end
