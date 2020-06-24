aa3091414a4bbb154e41f55495700e625e1ca7a6
bernard yohann <yohannb215@gmail.com>
2020-06-24 02:22:51 +0200

    repullmaster

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -5,6 +5,11 @@ class StepsController < ApplicationController
     @articles = Article.all
     @videos = Video.all
     @coachs = Coach.all
+
+    @question = Question.find(current_user.current_question_id)
+    @choice1 = Choice.where(question_id: current_user.current_question_id).first
+    @choice2 = Choice.where(question_id: current_user.current_question_id).last
+    @answer = Answer.new
   end
   
 end
